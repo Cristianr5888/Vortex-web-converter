9c616914c5da41f4a7ad3ded86d16d0204f96f65
Cristian Jimenez <cristian.vanced1858@gmail.com>
2024-10-06 21:27:31 -0700

    logo update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import "../styles/Copy.css";
-import crvancedd from "../assets/logo.png";
+import crvancedd from "../assets/weight-w-logo.png";
 function CopyR() {
   return (
     <div>
       <div className="bar">
         <p>Programmed by Cristian J. 2024</p>
         <div className="s">
-          <p>CRVANCED</p>
           <img className="img_copy" src={crvancedd} alt="" />
         </div>
       </div>
